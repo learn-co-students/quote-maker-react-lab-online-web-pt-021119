496e79bedc5355f33881e4aad8cfa0cdc5eb5b4a
cyantis <cyantis@gmail.com>
2019-11-18 06:41:45 -0700

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -41,7 +41,7 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal" onSubmit={ this.handleOnSubmit }>
+                <form className="form-horizontal" onSubmit={ this.handleOnSubmit } >
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
@@ -61,6 +61,7 @@ class QuoteForm extends Component {
                         type="text"
                         onChange={ this.handleAuthorChange }
                         value={this.state.author}
+                        name="author"
                       />
                     </div>
                   </div>
