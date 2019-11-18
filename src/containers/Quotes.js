3b5dcd45971e73087a1c877fdc1f64160852f620
cyantis <cyantis@gmail.com>
2019-11-18 06:39:28 -0700

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -5,7 +5,7 @@ import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
-  renderQuotes = () => this.props.quotes.map( quote => <QuoteCard quote={ quote } removeQuote={ this.props.removeQuote } upvoteQuote={ this.props.upvoteQuote } downvoteQuote={ this.props.downvoteQuote }/>);
+  renderQuotes = () => this.props.quotes.map((quote, idx) => <QuoteCard key={ idx } quote={ quote } removeQuote={ this.props.removeQuote } upvoteQuote={ this.props.upvoteQuote } downvoteQuote={ this.props.downvoteQuote }/>);
 
 
   render() {
